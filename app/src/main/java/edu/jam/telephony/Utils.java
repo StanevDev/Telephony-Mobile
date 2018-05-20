04e7ffea5347d9643001bd896da9a654caa172ba
Constantine <kostya.stanev@gmail.com>
2018-05-21 02:51:53 +0300

    Auth config implemented.

--- app/src/main/java/edu/jam/telephony/Utils.java
+++ app/src/main/java/edu/jam/telephony/Utils.java
@@ -2,11 +2,18 @@ package edu.jam.telephony;
 
 import java.math.BigDecimal;
 
+import io.reactivex.disposables.Disposable;
+
 public class Utils {
 
     public static String round(BigDecimal num){
         return num
                 .setScale(2, BigDecimal.ROUND_HALF_UP)
                 .toPlainString();
+    }
+
+    public static void dispose(Disposable disposable){
+        if (disposable != null && !disposable.isDisposed())
+            disposable.dispose();
     }
 }
--- app/src/main/java/edu/jam/telephony/network/RetrofitService.java
+++ app/src/main/java/edu/jam/telephony/network/RetrofitService.java
@@ -3,11 +3,16 @@ package edu.jam.telephony.network;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.Credentials;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -16,14 +21,18 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitService {
 
-    private static final String API_BASE_URL = "http://192.168.0.104/api/";
+    private static final String API_BASE_URL = "http://192.168.0.168:8080/api/";
 
     public static synchronized <S> S createApi(Class<S> serviceClass) {
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
+        AuthenticationInterceptor authInterceptor =
+                new AuthenticationInterceptor(getTestCredentials());
+
         OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(interceptor)
+                .addInterceptor(authInterceptor)
                 .connectTimeout(30, TimeUnit.SECONDS)
                 .readTimeout(30, TimeUnit.SECONDS)
                 .writeTimeout(30, TimeUnit.SECONDS)
@@ -44,6 +53,30 @@ public class RetrofitService {
 
         Retrofit retrofit = builder.build();
         return retrofit.create(serviceClass);
+    }
+
+    public static class AuthenticationInterceptor implements Interceptor {
+
+        private String authToken;
+
+        public AuthenticationInterceptor(String token) {
+            this.authToken = token;
+        }
+
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request original = chain.request();
+
+            Request.Builder builder = original.newBuilder()
+                    .header("Authorization", authToken);
+
+            Request request = builder.build();
+            return chain.proceed(request);
+        }
+    }
+
+    private static String getTestCredentials(){
+        return Credentials.basic("metzzo@comcast.net", "password");
     }
 
 }
